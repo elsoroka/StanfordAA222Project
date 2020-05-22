decbe9a074f7c8d5c9448780acc2e358f06945bc
Emiko Soroka <emiko@me.com>
2020-05-22 16:00:39 -0700

    Remove spaces in header row. Fixed adding columns for coreqs and related classes.

--- data/datacruncher.py
+++ data/datacruncher.py
@@ -40,7 +40,7 @@ def parseJsonData(infile, outfile):
 		# (if timeisTBA is true, there will be 0 meetings of 0 length)
 		# and number enrolled
 		# This makes the header row
-		outfile.write("dept, courseNumber, courseType, numberOfMeetings, meetingLengthHours, numberEnrolled\r\n")
+		outfile.write("dept,courseNumber,courseType,numberOfMeetings,meetingLengthHours,numberEnrolled,coreqWith,relatesTo\r\n")
 		
 		rawData = json.load(infile)
 		for divisionName in rawData.keys(): # iterate over division (grad/undergrad etc)
@@ -79,8 +79,8 @@ def writeCourseToFile(course, outfile):
 					section['courseType'],
 					str(numMeetings),
 					str(timeHours),
-					str(section['enrolled'])
-					]) + "\r\n")
+					str(section['enrolled']),
+					"",""]) + "\r\n")
 
 
 if __name__ == "__main__":
